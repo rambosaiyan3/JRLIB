df0edfa1d294f0b02fe353003d3d8844e14dd5d1
matheus.rambo.rca <matheus.rambo@operacao.rcadigital.com.br>
2018-10-27 21:56:26 -0300

    deleting test classes

--- src/org/com/ramboindustries/corp/sql/test/dao/EmployerDAO.java
+++
@@ -1,7 +0,0 @@
-package org.com.ramboindustries.corp.sql.test.dao;
-
-import org.com.ramboindustries.corp.sql.test.entity.Employer;
-
-public class EmployerDAO extends BaseDAO<Employer>{
-
-}
--- src/org/com/ramboindustries/corp/sql/test/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package org.com.ramboindustries.corp.sql.test.dao;
-
-import org.com.ramboindustries.corp.sql.test.entity.User;
-
-public class UserDAO extends BaseDAO<User>{
-
-}
