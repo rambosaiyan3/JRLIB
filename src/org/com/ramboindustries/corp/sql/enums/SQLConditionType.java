f7b75bf2a70e7ee408b4b01cbfd0e33801530902
Matheus Felipe Rambo <matheusrambo24@protonmail.com>
2018-11-17 18:05:44 -0200

    removing impossible conditions types

--- src/org/com/ramboindustries/corp/sql/enums/SQLConditionType.java
+++ src/org/com/ramboindustries/corp/sql/enums/SQLConditionType.java
@@ -3,14 +3,14 @@ package org.com.ramboindustries.corp.sql.enums;
 public enum SQLConditionType {
 
 	EQUAL(" = "),
+	DIFFERENT(" != "),
 	LIKE(" LIKE "),
-	BETWEEN(" BETWEEN "),
 	IS_NULL(" IS NULL "),
+	IS_NOT_NULL(" IS NOT NULL "),
 	GREATER_THAN(" > "),
 	LESS_THAN(" < "),
 	GREATER_THAN_OR_EQUAL(" >= "),
-	LESS_THAN_OR_EQUAL(" <= "),
-	IN(" IN ");
+	LESS_THAN_OR_EQUAL(" <= ");
 	
 	private String type;
 	
